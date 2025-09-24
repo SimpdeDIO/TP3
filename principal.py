d6319aec7bbdd71a21d6d29cb829b0eca654595f
Nicolas Merlos <140738721+SimpdeDIO@users.noreply.github.com>
2025-09-24 09:31:40 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,3 +1,34 @@
+import clase
+
+def cargar_envios(envios):
+    v = []
+    archivo = open("envios.csv")
+    for linea in archivo:
+        linea_1 = linea.split(",")
+        envio = pasaje_desde_csv(linea_1)
+        v.append(envio)
+    return v
+
+
+
+
+def pasaje_desde_csv(linea):
+    # 01|05|AB4F35
+    partes1 = linea[0].split("|")
+    print(partes1)
+    mod_origen = int(partes1[0])
+    mod_pago = int(partes1[1])
+    id_pago = partes1[2]
+    identificacion = linea[1]
+    nombre = linea[2]
+    tasa = float(linea[3])
+    monto = int(linea[4])
+    alg_comision = int(linea[5])
+    alg_impositivo = int(linea[6])
+    envio = clase.Envio(mod_origen, mod_pago, id_pago, identificacion, nombre, tasa, monto, alg_comision, alg_impositivo)
+    return envio
+
+
 def principal():
     v = []
     op = -1
@@ -8,7 +39,8 @@ def principal():
         op = int(input("Ingresa una opcionde del menu: "))
 
         if op == 1:
-            pass
+            v = cargar_envios("envios.csv")
+            print(v)
         elif op == 2:
             if v:
                 pass
