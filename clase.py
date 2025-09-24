aeffd4a46cd429884ca3a7d83865728548f0858f
Nicolas Merlos <140738721+SimpdeDIO@users.noreply.github.com>
2025-09-24 09:32:47 -0300

    Update clase.py

--- clase.py
+++ clase.py
@@ -1,6 +1,8 @@
 class Envio:
-    def __init__(self, cod, id, nom, tasa, mont, idcom, idimp):
-        self.codigo = cod
+    def __init__(self, cod_origen, cod_pago, id_pago, id, nom, tasa, mont, idcom, idimp):
+        self.codigo_origen = cod_origen
+        self.codigo_pago = cod_pago
+        self.identificador_pago = id_pago
         self.identificacion = id
         self.nombre = nom
         self.tasa = tasa
@@ -9,5 +11,5 @@ class Envio:
         self.alg_impositivo = idimp
 
     def __str__(self):
-        r = "Orden de pago: {:<5} - Identificacion: {:<5} - Destinatario: {:<5} - Tasa de conversion: {:<5} - Monto nominal: {:<5} "
-        return r.format(self.codigo, self.identificacion, self.nombre, self.tasa, self.monto)
+        r = "Moneda de Origen: {:<5}- Moneda de pago: {:<5} - Identificador de pago: {:<5} - Identificacion: {:<5} - Destinatario: {:<5} - Tasa de conversion: {:<5} - Monto nominal: {:<5} "
+        return r.format(self.codigo_origen, self.codigo_pago, self.identificador_pago, self.identificacion, self.nombre, self.tasa, self.monto)
